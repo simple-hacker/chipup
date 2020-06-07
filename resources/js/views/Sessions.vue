d01cb4e63db070aa8c2bc61d487dd231052baf19
Michael Perks <michael.perks@live.co.uk>
2020-06-07 15:13:56 +0100

    Displays for empty sessions

--- resources/js/views/Sessions.vue
+++ resources/js/views/Sessions.vue
@@ -13,11 +13,21 @@
 					<div class="text-white text-lg uppercase">Add New Session</div>
 				</div>
 			</router-link>
-            <div v-for="session in sessions"
-				:key="`${session.game_type}_${session.id}`"
-				class="col-span-4 md:col-span-2 xxl:col-span-1 mb-2 md:mb-0"
-			>
-                <session-summary :session="session"></session-summary>
+            <div
+                v-if="sessions.length > 0"
+            >
+                <div v-for="session in sessions"
+                    :key="`${session.game_type}_${session.id}`"
+                    class="col-span-4 md:col-span-2 xxl:col-span-1 mb-2 md:mb-0"
+                >
+                    <session-summary :session="session"></session-summary>
+                </div>
+            </div>
+            <div
+                v-else
+                class="col-span-4 flex bg-muted-dark border border-muted-dark shadow justify-center items-center my-2 p-4"
+            >
+                <div class="text-white uppercase">You do not have any sessions yet.</div>
             </div>
         </div>
     </div>
